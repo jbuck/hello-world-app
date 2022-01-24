dc1bbc65acf1ab217e907466afc9658607b204aa
Jon Buckley <jbuckley@mozilla.com>
2022-01-23 20:08:51 -0500

    Add support for address/port environment variables

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const address =  process.env.ADDRESS ?  process.env.ADDRESS : '127.0.0.1'
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
@@ -9,7 +11,7 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(port, address)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
